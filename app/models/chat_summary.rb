e8752c2f2916381badb32a7bca6df0fc70c932db
Jacob Green <jacob@nationbuilder.com>
2016-01-21 20:50:22 -0500

    Extract method

--- app/models/chat_summary.rb
+++ app/models/chat_summary.rb
@@ -10,25 +10,30 @@ class ChatSummary
     events.each do |event|
       event_hash[date_at_granularity(event.date, granularity)] << event
     end
-    summary_list = []
-    event_hash.each do |date, date_events|
-      summary_list << {'date' => date.iso8601, 'enters' => count_events(date_events, 'enter'), 'leaves' => count_events(date_events, 'leave'),
-                       'comments' => count_events(date_events, 'comment'), 'highfives' => count_events(date_events, 'highfive') }
-    end
-    summary_list
+    event_hash.map { |date, date_events| summary_json(date, date_events) }
   end
 
   private
-
-    def count_events(events, type)
-      events.count { |x| x.type == type }
-    end
 
     def date_at_granularity(time, granularity)
       return time.beginning_of_minute if granularity == 'minute'
       return time.beginning_of_hour if granularity == 'hour'
       return time.beginning_of_day if granularity == 'day'
       raise "Unknown granularity #{granularity}"
+    end
+
+    def summary_json(date, events)
+      {
+        'date' => date.iso8601,
+        'enters' => count_events(events, 'enter'),
+        'leaves' => count_events(events, 'leave'),
+        'comments' => count_events(events, 'comment'),
+        'highfives' => count_events(events, 'highfive')
+      }
+    end
+
+    def count_events(events, type)
+      events.count { |x| x.type == type }
     end
 
 end
